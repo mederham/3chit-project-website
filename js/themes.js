a1f8426d119da2210f4dd33803cea71e8708d53e
georg5174 <georg5174@users.noreply.github.com>
2021-11-26 07:46:25 +0100

    Changed references to root of repo

--- js/themes.js
+++ js/themes.js
@@ -6,11 +6,11 @@ function getThemeSettings() {
     
     if (config == 'light') {
         console.log("Loaded from storage: Light");
-        theme.setAttribute('href', '/style/light.css');
+        theme.setAttribute('href', '/3chit-project-website/style/light.css');
     }
     else if (config == 'dark') {
         console.log("Loaded from storage: Dark");
-        theme.setAttribute('href', '/style/dark.css');
+        theme.setAttribute('href', '/3chit-project-website/style/dark.css');
     }
     else {
         console.log("No theme saved - Using default (dark)");
@@ -28,13 +28,13 @@ function toggleTheme() {
     // Change the value of href attribute 
     // to change the css sheet.
     
-    if (theme.getAttribute('href') == '/style/light.css') {
-        theme.setAttribute('href', '/style/dark.css');
+    if (theme.getAttribute('href') == '/3chit-project-website/style/light.css') {
+        theme.setAttribute('href', '/3chit-project-website/style/dark.css');
         window.localStorage.setItem('theme', 'dark');
         console.log("Set theme: Dark");
     } 
     else {
-        theme.setAttribute('href', '/style/light.css');
+        theme.setAttribute('href', '/3chit-project-website/style/light.css');
         window.localStorage.setItem('theme', 'light');
         console.log("Set theme: Light");
     }
